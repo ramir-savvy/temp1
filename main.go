6fd11275a77c3e071069a27b7bc904fac370e9be
Raz Amir <88726761+ramir-savvy@users.noreply.github.com>
2023-09-20 11:40:24 +0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+  _ "github.com/DATA-DOG/go-sqlmock"
+  _ "github.com/lib/pq"
+	_ "go.uber.org/dig"
+  _ "github.com/google/uuid"
+	_ "github.com/mitchellh/mapstructure"
+	_ "github.com/stretchr/testify"
+	_ "github.com/urfave/cli/v2"
+	_ "gorm.io/gorm"
+)
